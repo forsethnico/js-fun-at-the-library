d284eca85914e4b6f06d6f2d261eb97006efcdf9
Nicole Forseth <forseth.nico@gmail.com>
2022-05-12 22:37:10 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return mainCharacter;
+}
+
+function saveReview(review, reviews) {
+  if(reviews.indexOf(review) === -1) {
+    reviews.push(review);
+  };
+  return reviews;
+}
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.length * 20;
+  return bookPageCount;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = (book.pageCount * 0.75);
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
+};
--- src/warm-up.js
+++ src/warm-up.js
@@ -13,51 +13,59 @@
 // var title = "Harry Potter";
 // var ...
 
-
+var title = "Moby Dick";
+var pages = 500;
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
 
-
+pages = (500 / 4);
 
 // Initialize a new variable "movie", but leave it undefined
 
-
+var movie = undefined;
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var sequel = title + " goes on an adventure";
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
-
+var isFavorite = true;
 
 // Create a new variable named "isShortStory", and use a comparison operator to
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
+ var isShortStory = pages < 25;
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
 
-
+if (pages < 1000) {
+  console.log("I could read that in a day!");
+} else {
+  console.log("Ok, maybe I could read that in a day and half.");
+}
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
 
-
+if (isFavorite === true) {
+  console.log(`My favorite book is ${title}!`);
+} else {
+  console.log(`I recently read the ${title} book`);
+}
 
 //------------------------- Arrays -------------------------//
 
@@ -65,17 +73,21 @@
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
 
+var books = ["DaVinci Code", "Tale of Two Cities", "To Kill a Mockingbird", "Scarlett's Letter", "Animal Farm"];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+books[2];
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
 
+var belowTwoHundred = [3, 6, 190, 78, 106, 35, 64, 155, 24, 83, 128, 46];
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+belowTwoHundred[4];
 
 //------------------------- Objects -------------------------//
 
@@ -83,6 +95,12 @@
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
 
+var dictionary = {
+  name: "Webster's",
+  hasThesaurus: false,
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720;
