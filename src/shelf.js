225259a069b9773b04004d2eb4eb72a56c1ca7dc
Nicole Forseth <forseth.nico@gmail.com>
2022-05-14 20:09:03 -0600

    Initial commit

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,45 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = "";
+  for (var i = 0; i < shelf.length; i++) {
+//Creating list of titles where last item in shelf array doesn't have comma
+    if (i === shelf.length - 1) {
+      titles = titles + `${shelf[i].title}`;
+    } else {
+      titles = titles + `${shelf[i].title}, ` ;
+    }
+  }
+  return titles;
+}
+
+function searchShelf(shelf, bookTitle) {
+  var isOnShelf = false
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+    isOnShelf = true;
+    } else {
+    isOnShelf = false;
+    }
+  }
+return isOnShelf;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
